74f8a128abe55eafb603420dc0d17b10fd803144
TeigenMillies <143120897+TeigenMillies@users.noreply.github.com>
2023-09-05 19:47:26 +0000

    PJ1

--- Proj 1/main.cpp
+++ Proj 1/main.cpp
@@ -4,23 +4,23 @@ using namespace std;
 void bubblesort(int arr[], int length)
 {
     int index1, index2;
-    bool change;
-    for (index1 = 0; index1 < length - 1; index1++)
+    
+    for (index1 = 0; index1 < length ; index1++)
     {
-        change = false;
-        for (index2 = 0; index2 < length - index1 - 1; index2++)
+        
+        for (index2 = 0; index2 < length - index1; index2++)
         {
             if (arr[index2] > arr[index2 + 1])
             {
                 //swap(arr[index2], arr[index2 + 1]);
+                
                 int temp = arr[index2];
                 arr[index2] = arr[index2 + 1];
                 arr[index2 + 1] = temp;
-                change = true;
+                
             }
         }
-        if (change == false)
-            break;
+        
     }
 }
 
