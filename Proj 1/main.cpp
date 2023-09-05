3501607733e05e56e8e48bf42d9a548bc10d49b6
TeigenMillies <143120897+TeigenMillies@users.noreply.github.com>
2023-09-05 19:50:48 +0000

    PJ1

--- Proj 1/main.cpp
+++ Proj 1/main.cpp
@@ -5,14 +5,14 @@ void bubblesort(int arr[], int length)
 {
     int index1, index2;
     
+    //Go through each element in array
     for (index1 = 0; index1 < length ; index1++)
     {
-        
         for (index2 = 0; index2 < length - index1; index2++)
         {
+            //swap bigger elements to the right
             if (arr[index2] > arr[index2 + 1])
             {
-                //swap(arr[index2], arr[index2 + 1]);
                 
                 int temp = arr[index2];
                 arr[index2] = arr[index2 + 1];
@@ -27,10 +27,10 @@ void bubblesort(int arr[], int length)
 void printSort(int arr[], int length)
 {
     int index;
+    //print each index of array
     for (index = 0; index < length; index++)
         cout << arr[index] << " ";
 
-    cout << endl;
 }
 
 int main()
