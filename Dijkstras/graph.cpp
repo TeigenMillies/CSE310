742d32657f9c33039781e9a4ab88a598e658e418
TeigenMillies <143120897+TeigenMillies@users.noreply.github.com>
2023-11-30 23:23:40 +0000

    Dijkstras

--- Dijkstras/graph.cpp
+++ Dijkstras/graph.cpp
@@ -61,51 +61,115 @@ bool Graph::loadGraph (const string& filename, const string& direction) {
         cerr << "Input file is empty" << endl;
         return false;
     }
+    // Close the file and reopen it to read the content again
+    file.close();
+    file.open(filename, ios::in);
 
+    // Check if the file is successfully reopened
+    if (!file) {
+        cerr << "Failed to reopen the graph file." << endl;
+        return false;
+    }
+
+    // Read the first line again to skip it
+    getline(file, line);
     
-
+    // Loop through the remaining lines to read edges and build adjacency lists
     while (getline(file, line)) {
-        int edgeId, startNode, endNode;
-        double weight;
+        int edgeId, startNode, endNode = 0;
+        double weight = 0;
         istringstream iss(line);
-        if (iss >> edgeId >> source >> endNode >> weight) {
+        // Parse the line to get edge information
+        if (iss >> edgeId >> startNode >> endNode >> weight) {
             startNode++;
             endNode++;
 
-            Edge* reverseEdge = new Edge;
-            reverseEdge->destination = endNode;
-            reverseEdge->weight = weight;
+            // Check if node IDs are valid
+            if (startNode > numVertices || endNode > numVertices || startNode < 1 || endNode < 1) {
+                cerr << "Invalid node IDs in the input file." << endl;
+                return false;
+            }
 
-            if (adjacencyLists[startNode]) {                //missing something
+            // Create a new edge
+            Edge* edge = new Edge;
+            edge->destination = endNode;
+            edge->weight = weight;
+
+            // Add edge to the adjacency list            
+            if (adjacencyLists[startNode] == nullptr) {                
                 adjacencyLists[startNode] = new Edge[numEdges];
             }
 
             int j = 0;
+            // Find the next available slot in the adjacency list
             while (adjacencyLists[startNode][j].destination != 0) {
                 j++;
             }
-            adjacencyLists[startNode][j] = *reverseEdge;
 
+            // Add the edge to the list
+            adjacencyLists[startNode][j] = *edge;
+
+            // If the graph is undirected, add the reverse edge
             if (direction == "undirected") {
                 Edge* reverseEdge = new Edge;
-                edge->destination = startNode;
-                edge->weight = weight;
+                reverseEdge->destination = startNode;
+                reverseEdge->weight = weight;
 
-                if (adjacencyLists[endNode]) {                //missing something
+                if (adjacencyLists[endNode] == nullptr) {
                 adjacencyLists[endNode] = new Edge[numEdges];
-            }
+                }
 
-            int j = 0;
-            while (adjacencyLists[endNode][j].destination != 0) {
-                j++;
+                j = 0;
+                // Find the next available slot in the adjacency list
+                while (adjacencyLists[endNode][j].destination != 0) {
+                    j++;
+                }
+                // Add the reverse edge to the list
+                adjacencyLists[endNode][j] = *reverseEdge;
+
+                // Deallocate memory for the reverse edge
+                free(reverseEdge);
             }
-            adjacencyLists[endNode][j] = *edge;
-            }
+            // Deallocate memory for the edge
+            free(edge);
 
         } else {
-            cerr << "Invalid Input" << endl;
+            cerr << "Invalid format for edge in the input file." << endl;
+            return false;
         }
         
     }
+    // Set the graph direction
+    if (direction == "undirected")
+        isDirected = false;
+    else
+        isDirected = true;
     
+    // Close the file
+    file.close();
+    return true;
+}
+
+void Graph::printAdjacencyLists() {
+    // Loop through each vertex in the graph
+    for (int v = 0; v < numVertices - 1; v++) {
+        cout << "Adjacency list for vertex " << v << ": ";
+
+        // Check if the adjacency list for the current vertex exists
+        if (adjacencyLists[v] != nullptr) {
+            int j = 0;
+
+            // Loop through each edge in the adjacency list
+            while (adjacencyLists[v][j].destination != 0) {
+                // Print the destination vertex and edge weight
+                cout << "(" << adjacencyLists[v][j].destination << ", " << adjacencyLists[v][j].weight << ") ";
+
+                //Move to the next edge in the list
+                j++;
+            }
+        }
+        // Print the predecessor value for the current vertex
+        cout << "Predecessor: " << predecessor[v];
+        cout << endl;
+    }
 }
